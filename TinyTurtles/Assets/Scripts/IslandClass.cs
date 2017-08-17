c3d9f3b1617bf4f351593e1b0d0f3f81872563b0
Cody Fulfer <Codeman1010@gmail.com>
2017-08-17 16:25:02 -0600

    Created seatiles
    Started up the first section of the map creation

---
+++ TinyTurtles/Assets/Scripts/IslandClass.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandClass : MonoBehaviour
+{
+    GameObject IslandGenerator;
+
+    [SerializeField]
+    int Q;//column
+    [SerializeField]
+    int R;//row
+    [SerializeField]
+    int S;//sum
+
+    //keeps neighbors in clockwise order
+    GameObject[] Neighbors = new GameObject[6];
+
+    [SerializeField]
+    static GameObject[] AllNodes;
+
+    //math for mapmaking
+    static float radius = 1f;
+    static float length = (radius * 2);
+    static float WIDTH_MATH = Mathf.Sqrt(3) / 2;
+    static float width = WIDTH_MATH * length;
+
+    public int GetCoords(string coord)
+    {
+        int ret;
+        if (coord == "Q") { ret = Q; }
+        else if (coord == "R") { ret = R; }
+        else if (coord == "S") { ret = S; }
+        else ret = 0;
+        return ret;
+    }
+
+
+    //return world space of this Hex
+    //Column = q, Row = r
+    public void SetPosition(int q, int r)
+    {
+        Q = q;
+        R = r;
+        S = -(q + r);
+        this.GetComponent<Transform>().position=new Vector3(width * (q + r / 2f), 0, (length * 0.75f) * r);
+    }
+
+    public void SetNeighbors()
+    {
+        AllNodes = GameObject.FindGameObjectsWithTag("SeaNode");
+        foreach (GameObject node in AllNodes)
+        {
+            Debug.Log(node);
+            //int nodeQ = node.GetComponentsInParent<IslandClass>().
+            //int nodeR = node.GetComponents<IslandClass>().R;
+            //int nodeS = node.GetComponents<IslandClass>().S;
+            //if (node.Q == Q && node.R == (R + 1)) { }
+        }
+    }
+
+    public void UpdateTileType(Material material, string typeNode)
+    {
+        this.GetComponentInChildren<MeshRenderer>().material = material;
+        this.GetComponentInChildren<Transform>().tag = typeNode;
+    }
+}
---
+++ TinyTurtles/Assets/Scripts/IslandGenerator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandGenerator : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject TerrainHexPrefab;
+    int mapSize = 100;
+    int LandSize = 32;
+    [SerializeField]
+    Material[] Materials;
+
+    private GameObject[] allLandNodes;
+    private GameObject[] allNodes;
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    //Generates the water grid with single island tile at its center.
+    private void GenerateMap()
+    {
+        for (int column = 0; column < mapSize; column++)
+        {
+            for (int row = 0; row < mapSize; row++)
+            {
+                //creates all the island nodes.
+
+                GameObject TerrainNode = (GameObject)
+                    Instantiate(
+                    TerrainHexPrefab,
+                    new Vector3(0,0,0),
+                    Quaternion.identity,
+                    this.transform
+                    );
+                TerrainNode.GetComponent<IslandClass>().SetPosition(column, row);
+                //Make the sea
+                TerrainNode.GetComponent<IslandClass>().UpdateTileType(Materials[0], "SeaNode");
+            }
+        }
+        //have all tiles find their neighbors
+        //allNodes = GameObject.FindGameObjectsWithTag("SeaNode");
+        //foreach (GameObject node in allNodes)
+        //{
+        //    node.GetComponent<IslandClass>().SetNeighbors();
+        //}
+        //Then make landTiles
+        //GenerateLandMass();
+    }
+
+    private void GenerateLandMass()
+    {
+        allLandNodes = GameObject.FindGameObjectsWithTag("LandNode");
+        while (allLandNodes.Length < LandSize)
+        {
+            GameObject targetLandNode = allLandNodes[Random.Range(0, allLandNodes.Length - 1)];  //get random tile from array.
+
+            //targetLandNode.GetComponent<IslandClass>().GetNeigbor();
+            //targetLandNode.GetComponent<IslandClass>().MakeLand(Materials[1]);
+
+            GameObject.FindGameObjectsWithTag("LandNode");
+        }
+    }
+}
