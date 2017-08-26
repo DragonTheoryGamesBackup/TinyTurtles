3e5a68b95d558b6cc637ecb30f5505a499e40698
Jason Thomas <codeguruinboise@gmail.com>
2017-08-26 17:14:32 -0600

    Panel Switching

---
+++ TinyTurtles/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+    private GameObject[] allMenuPanels;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+
+    public void SwitchPanel(GameObject destinationPanel)
+    {
+        Debug.Log(destinationPanel.ToString());
+
+        allMenuPanels = GameObject.FindGameObjectsWithTag("MenuPanel");
+        foreach (GameObject panel in allMenuPanels)
+        {
+            panel.SetActive(false);
+        }
+        destinationPanel.SetActive(true);
+    }
+}
---
+++ TinyTurtles/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject MainMenuPanel;
+
+    void Start ()
+    {
+        MainMenuPanel.GetComponentInChildren<ButtonController>().SwitchPanel(MainMenuPanel);
+    }
+}
